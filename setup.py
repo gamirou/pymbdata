fe49fc41c1669a9520bf3b6bc0d7dd40fcbec021
George Sima <bogdansima20@gmail.com>
2018-09-10 19:41:58 +0100

    Updating setup.py to setuptools

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name = "pymbdata",
-    version = "0.1",
-    packages = ['libs',],
-    license = open('LICENSE').read(),
-    long_description = open('README.md').read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymbdata",
+    version="0.1",
+    author="George Sima",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gamirou/pymbdata",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning"
+    ],
 )
