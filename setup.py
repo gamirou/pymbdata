a88728e0e195ebc7d929cd61a74c668ce11f0ed4
George Sima <bogdansima20@gmail.com>
2018-09-10 19:36:07 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = "pymbdata",
+    version = "0.1",
+    packages = ['libs',],
+    license = open('LICENSE').read(),
+    long_description = open('README.md').read()
+)
