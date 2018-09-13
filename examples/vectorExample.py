b0089a9b78ec32053213733be5345e777c4489c1
George Sima <bogdansima20@gmail.com>
2018-09-13 19:53:33 +0100

    Adding more data types for add function

--- examples/vectorExample.py
+++ examples/vectorExample.py
@@ -2,10 +2,9 @@ import sys, random
 sys.path.insert(0, r'.\libs')
 from vector import *
 
-v1 = Vector(6, 7, 5)
-print(v1.mag)
-v2 = Vector(4, 3, 2)
-v3 = v2 + v1
-print(v3)
-v3 += v1
-print(v3)
+vec1 = Vector(2, 4)
+vec2 = Vector(6, 5)
+vec1 = vec1 + 4
+print("Adding with scalar", vec1)
+vec1 = vec2 + (2, 4)
+print("Adding with tuple", vec1)
--- libs/vector.py
+++ libs/vector.py
@@ -15,30 +15,103 @@ class Vector:
     def copy(self):
         return Vector(self.x, self.y, self.z)
     
-    @staticmethod
-    def random2D():
+    @classmethod
+    def random2D(cls):
         return Vector(random.uniform(0, 1), random.uniform(0, 1))
 
-    @staticmethod
-    def random3D():
+    @classmethod
+    def random3D(cls):
         return Vector(random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1))
 
+    # @classmethod
+    # def normalize(cls, other):
+    #     return cls(other.x / other.mag, other.y / other.mag, other.z / other.mag)
+
+    # def normalize(self):
+    #     self.x /= self.mag
+    #     self.y /= self.mag
+    #     self.z /= self.mag
+
     def __str__(self):
-        lst = [self.x, self.z] if self.z == 0 else [self.x, self.y, self.z]
+        lst = [self.x, self.y] if self.z == 0 else [self.x, self.y, self.z]
         return str(tuple(lst))
 
     def __add__(self, other):
-        return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+        newX, newY, newZ = 0, 0, 0
+        if isinstance(other, int) or isinstance(other, float):
+            newX = self.x + other
+            newY = self.y + other
+            if self.z != 0:
+                newZ = self.z + other
+        elif isinstance(other, tuple) or isinstance(other, list):
+            # If 2D vector
+            if self.z == 0:
+                if len(other) == 2:
+                    newX = self.x + other[0]
+                    newY = self.y + other[1]
+                else:
+                    raise ValueError("Your list has to be of length 2.")
+            # If 3D vector
+            else:
+                if len(other) == 3:
+                    newX = self.x + other[0]
+                    newY = self.y + other[1]
+                    newZ = self.z + other[2]
+                else:
+                    raise ValueError("Your list has to be of length 3.")
+        elif isinstance(other, Vector):
+            newX = self.x + other.x
+            newY = self.y + other.y
+            newZ = self.z + other.z
+        else:
+            raise TypeError("Your input has to be a vector, a scalar, a list or a tuple.")
+
+        return Vector(newX, newY, newZ)
 
     def __iadd__(self, other):
-        self.x += other.x
-        self.y += other.y
-        self.z += other.z
+        if isinstance(other, int) or isinstance(other, float):
+            self.x += other
+            self.y += other
+            if self.z != 0:
+                self.z += other
+        elif isinstance(other, tuple) or isinstance(other, list):
+            # If 2D vector
+            if self.z == 0:
+                if len(other) == 2:
+                    self.x += other[0]
+                    self.y += other[1]
+                else:
+                    raise ValueError("Your list has to be of length 2.")
+            # If 3D vector
+            else:
+                if len(other) == 3:
+                    self.x += other[0]
+                    self.y += other[1]
+                    self.z += other[2]
+                else:
+                    raise ValueError("Your list has to be of length 3.")
+        elif isinstance(other, Vector):
+            self.x += other.x
+            self.y += other.y
+            self.z += other.z
+        else:
+            raise TypeError("Your input has to be a vector, a scalar, a list or a tuple.")
+
         return self
 
+    # def __sub__(self, other):
+    #     return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    # def __isub__(self, other):
+    #     self.x -= other.x
+    #     self.y -= other.y
+    #     self.z -= other.z
+    #     return self
+    
+
 """
+normalize()
 fromAngle() 	Make a new 2D unit vector from an angle
-add() 	Adds x, y, and z components to a vector, one vector to another, or two independent vectors
 sub() 	Subtract x, y, and z components from a vector, one vector from another, or two independent vectors
 mult() 	Multiply a vector by a scalar
 div() 	Divide a vector by a scalar
